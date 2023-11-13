47ead3724751b0ba864e13297422e329e6931a2a
EduSerodio <85908668+EduSerodio@users.noreply.github.com>
2023-11-13 16:37:50 -0300

    modificando metodos de create e update da ProdutoService.cs

--- SustentaMais/Service/Implements/ProdutoService.cs
+++ SustentaMais/Service/Implements/ProdutoService.cs
@@ -61,7 +61,7 @@ namespace SustentaMais.Service.Implements
                 produto.Categoria = BuscaCategoria;    
             }
 
-            produto.Categoria = produto.Categoria is not null ? _context.Categorias.FirstOrDefault(p => p.Id == produto.Categoria.Id) : null;
+            produto.User = produto.User is not null ? _context.Users.FirstOrDefault(u => u.Id == produto.User.Id) : null;
 
             await _context.Produtos.AddAsync(produto);
             await _context.SaveChangesAsync();
@@ -85,7 +85,7 @@ namespace SustentaMais.Service.Implements
 
             }
 
-            produtos.Categoria = produtos.Categoria is not null ? _context.Categorias.FirstOrDefault(t => t.Id == produtos.Categoria.Id) : null; // verificação
+            produtos.User = produtos.User is not null ? _context.Users.FirstOrDefault(t => t.Id == produtos.User.Id) : null; // verificação
 
             _context.Entry(ProdutoUpdate).State = EntityState.Detached; 
             _context.Entry(produtos).State = EntityState.Modified; 
